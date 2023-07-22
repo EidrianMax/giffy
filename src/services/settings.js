ff5274c55b7953ffd8dd8797f66b220e84fb836a
Adrian <eidrian.uct@gmail.com>
2023-07-22 17:58:06 +0200

    change api url

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,6 +1,6 @@
 const API_KEY = 'tIa38746S6zKMMohYuwnZxslq6HOqp2b'
 const API_URL_GIPHY = 'https://api.giphy.com/v1'
-const API_URL_USERS = 'https://giffy-production.up.railway.app/api'
+const API_URL_USERS = 'https://giffy-api-nr77.onrender.com/api'
 
 export {
   API_KEY,
