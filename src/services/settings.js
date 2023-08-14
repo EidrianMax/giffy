c7f0fac197e4bdbd1af7ee2323522add3b70532a
Adrian <eidrian.uct@gmail.com>
2023-08-14 17:52:13 +0200

    change api url

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,6 +1,6 @@
 const API_KEY = 'tIa38746S6zKMMohYuwnZxslq6HOqp2b'
 const API_URL_GIPHY = 'https://api.giphy.com/v1'
-const API_URL_USERS = 'https://zl9hkm-8001.csb.app'
+const API_URL_USERS = 'https://giffy-api-n5gt.onrender.com'
 
 export {
   API_KEY,
